d8465689cae71a4f4d7240809a6ec559ce92535d
MarlonCopara4120 <mcopara4120@uta.edu.ec>
2022-01-03 23:25:13 -0500

    Primer Commit

---
+++ OneDrive/Documentos/NetBeansProjects/Mundo/src/Mundo/Mundo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Mundo;
+
+/**
+ *
+ * @author copar
+ */
+public class Mundo {
+    public static void main(String[] args) {
+        System.out.println("HOLA WORLD");
+    }
+}
