708f042cb19202de1506b265d5459b4d535ec726
MarlonCopara4120 <mcopara4120@uta.edu.ec>
2022-01-04 22:14:44 -0500

    Se añadió un método guardar.

---
+++ OneDrive/Documentos/NetBeansProjects/ProyectoGestionRegistroHospital/src/Metodos_SQL/Metodos_SQL.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Metodos_SQL;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author copar
+ */
+public class Metodos_SQL {
+
+    public static PreparedStatement sentencia_preparada;
+
+    public int guardar(String nombre, String apellidos, String servicio, String usuario, String contraseña) throws SQLException {
+        int resultado = 0;
+        Connection conexion = null;
+
+        String sentencia_guardar = ("INSERT INTO login (Nombre,Apellido,Servicio,Usuario,Contraseña)VALUES(?,?,?,?,?)");
+
+        try {
+            conexion = ConexionBD.conectar();
+            sentencia_preparada = conexion.prepareStatement(sentencia_guardar);
+            sentencia_preparada.setString(1, nombre);
+            sentencia_preparada.setString(2, apellidos);
+            sentencia_preparada.setString(3, servicio);
+            sentencia_preparada.setString(4, usuario);
+            sentencia_preparada.setString(5, contraseña);
+
+            resultado = sentencia_preparada.executeUpdate();
+            sentencia_preparada.close();
+            conexion.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return resultado;
+    }
+}
