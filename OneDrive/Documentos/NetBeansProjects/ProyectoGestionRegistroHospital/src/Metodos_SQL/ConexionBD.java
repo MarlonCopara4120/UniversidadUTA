5b59f782f3afb65ae8898ff774be912a0062dd7b
MarlonCopara4120 <mcopara4120@uta.edu.ec>
2022-01-04 21:53:37 -0500

    Se creó una clase para crear la conexión con la base de datos.

---
+++ OneDrive/Documentos/NetBeansProjects/ProyectoGestionRegistroHospital/src/Metodos_SQL/ConexionBD.java
@@ -0,0 +1,26 @@
+
+package Metodos_SQL;
+
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBD {
+    
+  public static String url = "jdbc:mysql://localhost/proyectobd";
+    public static String usuario = "hospital";
+    public static String contraseña = "hospital123";
+    public static String clase = "com.mysql.jdbc.Driver";
+
+    public static Connection conectar() {
+        Connection conexion = null;
+        try {
+            Class.forName(clase);
+            conexion = (Connection) DriverManager.getConnection(url, usuario, contraseña);
+            System.out.println("Conexion establecida");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+        }
+        return conexion;
+    }
+}
